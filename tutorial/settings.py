fb29cbc134120d6a5ec37651e5df2bf59a17b4d6
VladRadigin <angry.gamer.comi@gmail.com>
2022-02-13 21:48:36 +0600

    Correcting the path to the templates directory

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,9 +42,7 @@ ROOT_URLCONF = 'tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tutorial/views.py
+++ tutorial/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse, response
 from django.shortcuts import render
 
 def first_page(request):
-    a = '<h1>Hi there!</h1>'
-    text = '<p>Some text...</p>'
-    return render(request, '/index.html', {
+    a = 'Hi there!'
+    text = 'Some text...'
+    return render(request, './index.html', {
         'a': a,
         'text': text
     })
